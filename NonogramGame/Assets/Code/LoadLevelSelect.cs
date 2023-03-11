94b91ae5b783fcf93c531db23d8f348087452332
Emma Erjanne <e.erjanne@gmail.com>
2023-03-11 21:26:31 +0200

    nothing works, life is hell + got us tmp

---
+++ NonogramGame/Assets/Code/LoadLevelSelect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Nonogram
+{
+    public class LoadLevelSelect : MonoBehaviour
+    {
+        // Start is called before the first frame update
+       public void PlayGame ()
+       {
+            SceneManager.LoadScene(1);
+       }
+    }
+}
