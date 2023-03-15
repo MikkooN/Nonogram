3bdb8526bd871d03242618af8619dc5b2e508c66
Mikko <mikko.nystedt@tuni.fi>
2023-03-15 18:24:26 +0200

    New touch input

--- NonogramGame/Assets/Code/TouchInput.cs
+++ NonogramGame/Assets/Code/TouchInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Nonogram
 {
@@ -8,7 +9,39 @@ namespace Nonogram
     {
         public static GameObject tappedSquare;
 
-        void Update()
+        private PlayerInput playerInput;
+        private InputAction tap;
+        private Vector3 worldTouchPosition;
+
+        private void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            tap = playerInput.actions.FindAction("Tap");
+        }
+
+        private void OnEnable()
+        {
+            tap.performed += Tapped;
+        }
+
+        private void OnDisable()
+        {
+            tap.performed -= Tapped;
+        }
+
+        private void Tapped(InputAction.CallbackContext context)
+        {
+            Vector3 tapPosition = Camera.main.ScreenToWorldPoint(tap.ReadValue<Vector2>());
+
+            /*Vector2 touchPosition = context.ReadValue<Vector2>();
+            Vector3 screenCoordinate = touchPosition;
+            this.worldTouchPosition = Camera.main.ScreenToWorldPoint(screenCoordinate);*/
+
+            Debug.Log(tapPosition);
+        }
+
+
+        /*void Update()
         {
             if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
             {
@@ -20,6 +53,6 @@ namespace Nonogram
                     tappedSquare = raycastHit.collider.gameObject;
                 }
             }
-        }
+        }*/
     }
 }
