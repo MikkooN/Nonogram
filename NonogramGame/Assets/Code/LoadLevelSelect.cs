ff67cc42e1664bdd69a11ca12c578c8ea2f3014b
Emma Erjanne <e.erjanne@gmail.com>
2023-03-23 19:34:16 +0200

    Simplified scene management code, win n lose scenarios buttons half done

--- NonogramGame/Assets/Code/LoadLevelSelect.cs
+++ NonogramGame/Assets/Code/LoadLevelSelect.cs
@@ -7,9 +7,25 @@ namespace Nonogram
 {
     public class LoadLevelSelect : MonoBehaviour
     {
-       public static void LoadLevel()
+       public static void LoadNextLevel()
        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
        }
+
+       public static void LoadMainMenu()
+       {
+            SceneManager.LoadScene("MainMenu");
+       }
+
+       public void QuitGame ()
+      {
+        Debug.Log ("Quit the game!");
+        Application.Quit();
+      }
+
+      public void ReplayLevel ()
+      {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      }
     }
 }
