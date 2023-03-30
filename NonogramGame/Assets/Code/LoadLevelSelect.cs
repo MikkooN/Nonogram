cb5f3e391a1607715603c107f6af92941f4fc6e2
Emma Erjanne <e.erjanne@gmail.com>
2023-03-30 13:20:03 +0300

    Added updated 15x15 food, unlocking levels works but needs work

--- NonogramGame/Assets/Code/LoadLevelSelect.cs
+++ NonogramGame/Assets/Code/LoadLevelSelect.cs
@@ -2,11 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Nonogram
 {
     public class LoadLevelSelect : MonoBehaviour
     {
+        int levelsUnlocked;
+
+        public Button[] buttons;
+        
+
+        void Start()
+        {
+            levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked", 1);
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].interactable = false;
+            }
+
+            for (int i = 0; i < levelsUnlocked; i++)
+            {
+                buttons[i].interactable = true;
+            }
+        
+        }
+
+        public void levelUnlock()
+        {
+            int currentLevel = SceneManager.GetActiveScene().buildIndex;
+
+            if(currentLevel >= PlayerPrefs.GetInt("levelsUnlocked"))
+            {
+                PlayerPrefs.SetInt("levelsUnlocked", currentLevel +1);
+            }
+
+            Debug.Log("LEVEL" + PlayerPrefs.GetInt("levelsUnlocked") + "UNLOCKED");
+        }
+
+        public void LevelSelect(int levelIndex)
+        {
+            SceneManager.LoadScene(levelIndex);
+        }
+        
         public static void LoadNextLevel()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
@@ -29,5 +68,6 @@ namespace Nonogram
             SquareCheck.ResetLevel();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        
     }
 }
